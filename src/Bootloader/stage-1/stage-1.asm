2eadf5b495614e73cca069eff05afbbe398492b8
Lior Vazana <liorvazana12@gmail.com>
2023-07-21 07:50:43 -0400

    Added GDT and enabled a20 line

--- src/Bootloader/stage-1/stage-1.asm
+++ src/Bootloader/stage-1/stage-1.asm
@@ -7,20 +7,34 @@ main:
 	mov ds, ax
 	mov es, ax
 
+	; save the disk we booted from for later use
+	mov [CURR_DISK], dl 
+	
 	; setup stack
 	mov ss, ax
 	mov sp, 0x7c00
 
+	; load stage-2 from disk to RAM
+	mov ah, 02h            						    ; read mode
+	mov dl, [CURR_DISK]   							; disk index
+	mov al, (stage_2_end - stage_2_start) / 512     ; size of stage-2
+	mov ch, 0 									    ; first cylinder
+	mov dh, 0                                       ; first head
+	mov bx, 0x7c00 + 512                            ; sector address 
+	int 13h										    ; disk interrupt
+
+	; enter stage-2
 	mov bx, JUMP_MSG
 	call print_string
-	
+
 	jmp stage_2_start
 
 
 
 %include "./src/Bootloader/stage-1/strings_functions.asm"
 
-JUMP_MSG db "Jumping to stage 2!"
+JUMP_MSG db "Jumping to stage 2!", 0
+CURR_DISK db 0
 
 times 510 - ($-$$) db 0
 db 0x55
--- src/Bootloader/stage-2/stage-2.asm
+++ src/Bootloader/stage-2/stage-2.asm
@@ -1,1 +1,13 @@
+stage_2_init:
+	; init GDT
+	mov word ptr [GDT], 0xffff
+	add word ptr [GDT + 6], 0xf
 
+	; update the GDT address
+	lgdt GDT
+
+	; Call the BIOS function to enable the A20 line
+    mov ax, 0x2401       ; Function 0x2401 - Enable A20 line
+    int 0x15            ; Call BIOS interrupt 0x15
+    
+GDT dw 4 dup(0)
